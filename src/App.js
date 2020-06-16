fcb7e8d06ba814d161db25a2fc96a9e9a0c18e34
Dmitry Kovalev <trigger1610@gmail.com>
2020-06-16 18:40:23 +0400

    configure project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
-import MainScreen from './MainScreen';
+import React from "react";
+import { StatusBar } from "react-native";
+import MainScreen from "./MainScreen";
 
 const App = () => {
   return (
     <React.Fragment>
       <StatusBar barStyle="dark-content" />
-      <MainScreen/>
+      <MainScreen />
     </React.Fragment>
   );
 };
